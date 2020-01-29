3f9fa5aa0a67d5a8d9a4bf11ff64ee2207d9aa1f
Matheus Filipe <mfcr031@gmail.com>
2020-01-29 12:06:52 -0300

    validação e atualização de recipients

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -1,10 +1,23 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
-    /**
-     * Fazer as checagens com o Yup
-     */
+    const schema = Yup.object().shape({
+      dest_name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      cep_number: Yup.string()
+        .required()
+        .min(8),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
 
     const recipients = await Recipient.create(req.body);
 
@@ -12,7 +25,29 @@ class RecipientController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+      dest_name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      state: Yup.string(),
+      city: Yup.string(),
+      cep_number: Yup.string().min(8),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const user = await Recipient.findByPk(req.body.id);
+
+    /**
+     * Verificar se o usuário logado é admin
+     */
+
+    const recipient = await user.update(req.body);
+
+    return res.json(recipient);
   }
 }
 
